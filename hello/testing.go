3f1aef06c09b9fe00aeab4317dd23ed8659f3ba7
vaqle <92319390+vaqle@users.noreply.github.com>
2022-03-15 21:52:42 -0400

    Update testing.go

--- hello/testing.go
+++ hello/testing.go
@@ -6,15 +6,18 @@ import (
 )
 
 func main() {
+	//switchWithoutCondition()
+	//workWithPointers()
+	//lol()
+	createAp()
+}
+
+func switchWithoutCondition() {
 	sum := 1
 	for sum < 100 {
 		sum += sum
 	}
 	println(sum)
-	switchWithoutCondition()
-}
-
-func switchWithoutCondition() {
 	fmt.Println("Executing function")
 	defer fmt.Println("Function executed")
 	t := time.Now()
@@ -24,4 +27,47 @@ func switchWithoutCondition() {
 	default:
 		fmt.Println("IDK")
 	}
+}
+
+func workWithPointers() {
+
+	var p *int
+	i := 42
+	p = &i //generate its operand
+	//* denotes the pointers underlying value
+	//so if i did this
+	fmt.Println(p) //output would be 0xc000016088
+	//however if i did this
+	fmt.Println(*p) //output is 42
+
+	//if we wanted to update p we would do this
+	*p = 21     //this is dereferencing or inderecting
+	println(*p) //outputs 21
+	println(&i) //outputs addresss
+}
+
+func createAp() {
+	var i, j int = 20, 25
+	k := &j
+	p := &i    //establish a pointer
+	println(p) //should output adresss
+	println(k) //should output adresss
+	//now if we want to get i we would do this
+	result := *p
+	fmt.Println("Result of I:", result)
+	*p = 10
+	println(*p) //should be 10
+
+}
+func lol() {
+	i, j := 42, 2701
+
+	p := &i         // point to i
+	fmt.Println(*p) // read i through the pointer
+	*p = 21         // set i through the pointer
+	fmt.Println(i)  // see the new value of i
+
+	p = &j         // point to j
+	*p = *p / 37   // divide j through the pointer
+	fmt.Println(j) // see the new value of j
 }
